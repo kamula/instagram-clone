1a0b1dad274232b116901fd42d25df9f9a711282
kamula <kamulaisaac@gmail.com>
2020-10-13 16:29:15 +0300

    login update

--- instagram/accounts/forms.py
+++ instagram/accounts/forms.py
@@ -18,7 +18,8 @@ class AccountAuthenticationForm(forms.ModelForm):
         fields = ('email','password')
 
     def clean(self):
-        email = self.cleaned_data['email']
-        password = self.cleaned_data['password']
-        if not authenticate(email = email,password = password):
-            raise forms.ValidationError('invalid login credentials')
+        if self.is_valid():
+            email = self.cleaned_data['email']
+            password = self.cleaned_data['password']
+            if not authenticate(email = email,password = password):
+                raise forms.ValidationError('invalid login credentials')
